5e904c699d40c01f65d1ca3bed2c612f25901358
vinodinii24 <vinodinii024@gmail.com>
2026-08-01 23:09:53 +0530

    My local changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-//import React, { useEffect } from "react";
-//import { motion, useScroll, useSpring } from "framer-motion";
+import React, { useEffect } from "react";
+import { motion, useScroll, useSpring } from "framer-motion";
 import Lenis from "lenis";
 
 // Living 3D Animated Background Engine
